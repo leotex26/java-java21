796ebd6db0cc5f33ee65ec0293e006cbcfa4c33a
leotex26 <160853319+leotex26@users.noreply.github.com>
2025-12-10 16:07:16 +0100

    exo localDate

--- src/test/java/java17/ex04/DateAndTime_04_Test.java
+++ src/test/java/java17/ex04/DateAndTime_04_Test.java
@@ -3,6 +3,7 @@ package java17.ex04;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 
@@ -18,15 +19,15 @@ public class DateAndTime_04_Test {
     public void test_localDateTime_of() {
 
         // TODO créer un objet LocalDateTime à la date heure 12h00m00s 01/01/2018
-        LocalDateTime result = null;
+        LocalDateTime localDateTime = LocalDateTime.of(2018, 1,1,12,0,0);
 
         // TODO valoriser les différentes variables afin de rendre le test passant
-        int hour = 0;
-        int minutes = 0;
-        int second = 0;
-        int year = 0;
-        Month month = null;
-        int dayOfMonth = 0;
+        int hour = localDateTime.getHour();
+        int minutes = localDateTime.getMinute();
+        int second = localDateTime.getSecond();
+        int year = localDateTime.getYear();
+        Month month = localDateTime.getMonth();
+        int dayOfMonth = localDateTime.getDayOfMonth();
 
         assertThat(hour, is(12));
         assertThat(minutes, is(0));
@@ -40,16 +41,16 @@ public class DateAndTime_04_Test {
     public void test_localDateTime_parse() {
 
         // TODO créer un objet LocalDateTime à l'heure 2 mars 2009 à 09h30m00s à l'aide de la méthode parse
-        LocalDateTime result = null;
+      LocalDateTime localDateTime = LocalDateTime.parse("2009-03-02T09:30:00");
 
         // TODO valoriser les différentes variables afin de rendre le test passant
-        int hour = 0;
-        int minutes = 0;
-        int second = 0;
+        int hour = localDateTime.getHour();
+        int minutes = localDateTime.getMinute();
+        int second = localDateTime.getSecond();
 
-        int year = 0;
-        Month month = null;
-        int dayOfMonth = 0;
+        int year = localDateTime.getYear();
+        Month month = localDateTime.getMonth();
+        int dayOfMonth = localDateTime.getDayOfMonth();
 
         assertThat(year, is(2009));
         assertThat(month, is(Month.MARCH));
@@ -65,10 +66,10 @@ public class DateAndTime_04_Test {
 
         // TODO créer un objet LocalDateTime le 27/11/2017 à 12h00m00s
         // TODO utiliser la méthode of
-        LocalDateTime localDateTime = null;
+        LocalDateTime localDateTime = LocalDateTime.of(2017,11,27,12,0,0);
 
         // TODO Formatter l'heure pour que le test soit passant
-        String result = null;
+        String result = localDateTime.format(DateTimeFormatter.ofPattern("yyyy - MM/dd HH:mm"));
 
         assertThat(result, is("2017 - 11/27 12:00"));
     }
--- src/test/java/java17/ex07/DateAndTime_07_Test.java
+++ src/test/java/java17/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,11 @@ package java17.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,13 +21,16 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(2017, Calendar.FEBRUARY, 12);
+    Date date = calendar.getTime();
 
 		// TODO transformer la date en Instant
+    Instant instant = date.toInstant();
 
 		// TODO transformer la date en LocalDate
 
-		LocalDate result = null;
+    LocalDate result = instant.atZone(ZoneId.systemDefault()).toLocalDate();
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
